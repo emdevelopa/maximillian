089b2978ebde7f8b2c238a23a71a3aaeb2d79714
emdevelopa <gyimahsunday@gmail.com>
2023-12-27 12:33:49 +0100

    good commit

---
+++ src/home.jsx
@@ -0,0 +1,120 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+export default function Home(props) {
+  const [onHover, setOnHover] = useState(false);
+  const showDropdown = () => {
+    setOnHover(!onHover);
+  };
+  const hideDropdown = () => {
+    setOnHover(!onHover);
+  };
+  return (
+    <>
+      <div className="bgImage">
+        <nav className="text-white fixed w-full">
+          <ul className="flex justify-around py-4 items-center">
+            <li>
+              <a href="#" className="hover:text-orange-400">
+                ABOUT
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-orange-400"
+                onMouseOver={showDropdown}
+                onMouseLeave={hideDropdown}
+              >
+                STORES <FontAwesomeIcon icon="caret-down" />
+              </a>
+
+              <ul
+                className={
+                  "absolute bg-[#0000009e] w-[6em] p-2 rounded " +
+                  (onHover ? "block" : "hidden")
+                }
+              >
+                <li>PET</li>
+                <li>GADGET</li>
+                <li>WEARS</li>
+                <li>BOOKS</li>
+                <li></li>
+              </ul>
+            </li>
+            <li>
+              <img src={props.logo} alt="swsw" className="w-[4em]" />
+              {/* <div className="h-[4em] w-[4em] rounded-[50%] bg-blue-700"></div> */}
+            </li>
+            <li>
+              <a href="#" className="hover:text-orange-400">
+                CARS
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-orange-400">
+                A&E
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="bg-[#00000071] text-white h-[90vh] flex justify-center items-center">
+          <div className="flex flex-col items-center">
+            <h1 className="text-[65px] font-bold">The Great Outdoors</h1>
+            <p>Wander often, Wonder always</p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-black text-white pt-[6em]">
+        <div className="flex items-center flex-col gap-4 mb-12">
+          <h1 className="text-[36px]">Explore the World</h1>
+          <p className="text-center w-1/2">
+            We seek to provide the most authentic content from athletes,
+            adventures, explorers and travellers around the world. Our long-term
+            mission is to educate inspire and enable all peoples to experience &
+            protect wilderness.
+          </p>
+        </div>
+        <div className="px-[8em] flex justify-between mb-12">
+          <div className="w-[20em] h-[25em] bg-blue-500"></div>
+          <div className="w-[20em] h-[25em] bg-[orange]"></div>
+          <div className="w-[20em] h-[25em] bg-blue-500"></div>
+        </div>
+        <p className="text-center text-[14px] text-blue-600">SEE MORE &gt;</p>
+      </div>
+      <div className="bg-black text-white pt-[6em]">
+        <div className="flex items-center flex-col gap-4 mb-12">
+          <h1 className="text-[36px]">The Journal</h1>
+          <p className="text-center w-1/2">
+            Our favorite stories about public lands and opportunities for you to
+            get involved in protecting your outdoor experience
+          </p>
+        </div>
+        <div className="px-[8em] flex justify-between mb-12">
+          <div className="flex flex-col items-center gap-4">
+            <div className="w-[30em] h-[20em] bg-blue-500"></div>
+            <p>MAY 30, 2017</p>
+            <h1 className="font-bold text-[24px]">An Unforgettable</h1>
+            <p className="text-center mt-4 ">
+              If you have one day to visit Yosemite National Park <br /> and you
+              want to make the most out of it.
+            </p>
+          </div>
+          <div className="flex flex-col items-center gap-4">
+            <div className="w-[30em] h-[20em] bg-blue-500"></div>
+            <p>MAY 30, 2017</p>
+            <h1 className="font-bold text-[24px]">An Unforgettable</h1>
+            <p className="text-center mt-4 ">
+              If you have one day to visit Yosemite National Park <br /> and you
+              want to make the most out of it.
+            </p>
+          </div>
+        </div>
+        <p className="text-center text-[14px] text-blue-600">ALL POSTS &gt;</p>
+      </div>
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>
+    </>
+  );
+}
