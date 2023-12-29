8572463bd4983ff75e0f239ee223c2e64ef680bb
emdevelopa <gyimahsunday@gmail.com>
2023-12-29 23:12:58 +0100

    nice commit

--- src/home.jsx
+++ src/home.jsx
@@ -200,6 +200,27 @@ export default function Home(props) {
       </div>
       <footer className="bgImage h-[20em] flex items-end text-white justify-center">
         <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
+      </footer>{" "}
+      <footer className="bgImage h-[20em] flex items-end text-white justify-center">
+        <p>&copy; 2023 MAXIMILLIAN All rights reserved</p>
       </footer>
     </>
   );
