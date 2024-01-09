18f52d07f47bd3e70f1d9fce422a259e9e60464a
emdevelopa <gyimahsunday@gmail.com>
2024-01-09 07:56:07 +0100

    good

--- src/components/journal.jsx
+++ src/components/journal.jsx
@@ -15,7 +15,6 @@ export default function Journal(props) {
         </div>
         <div className="px-[8em] flex justify-center max-md-[600px]:flex-col">
           <img src={props.journalImage} alt="index" className="w-[50%]" />
-          bn
           {/* <img src={props.images} className="w-[50%] h-[40em]" alt="hello" /> */}
 
           {/* {props.images.slice.map((index,item) => {
