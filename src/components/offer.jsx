ec0a7b24f9677d2ee13e4ed2bc48503335f5736d
emdevelopa <gyimahsunday@gmail.com>
2024-01-10 16:27:34 +0100

    yes

--- src/components/offer.jsx
+++ src/components/offer.jsx
@@ -1,11 +1,65 @@
 export default function WhatWeOffer() {
   return (
     <section className="text-white">
-      <div className=" bg-white">
-        <div className="bg-[#F94C30] p-[4em]">ddd</div>
-
-        <h1>01.</h1>
+      <div className="text-center">
+        <h1 className="font-bold text-[45px] m-auto w-[50%]">
+          Best Services We Can Offer For You !
+        </h1>
       </div>
+      <section className=" p-[6em] flex justify-between">
+        <div className="border relative border-gray-400 rounded-[2em] w-[16em] p-[2em]">
+          <div className="bg-[#F94C30] absolute top-[-10%] rounded-[1em] h-[4em] w-[4em]"></div>
+          <div className="flex justify-end">
+            <h1 className="text-[40px] font-bold">01.</h1>
+          </div>
+          <h2 className="font-bold text-[22px] mb-6">
+            Website Develop Planning
+          </h2>
+          <p>
+            Illo tempor unde ornare ipsum vivamus facilis? Tempus mollit secate
+            inn.
+          </p>
+        </div>
+        <div className="border relative border-gray-400 rounded-[2em] w-[16em] p-[2em]">
+          <div className="bg-[#F94C30] absolute top-[-10%] rounded-[1em] h-[4em] w-[4em]"></div>
+          <div className="flex justify-end">
+            <h1 className="text-[40px] font-bold">02.</h1>
+          </div>
+          <h2 className="font-bold text-[22px] mb-6">
+            Graphic & Sketch Designing
+          </h2>
+          <p>
+            Illo tempor unde ornare ipsum vivamus facilis? Tempus mollit secate
+            inn.
+          </p>
+        </div>
+        <div className="border relative border-gray-400 rounded-[2em] w-[16em] p-[2em]">
+          <div className="bg-[#F94C30] absolute top-[-10%] rounded-[1em] h-[4em] w-[4em]"></div>
+          <div className="flex justify-end">
+            <h1 className="text-[40px] font-bold">03.</h1>
+          </div>
+          <h2 className="font-bold text-[22px] mb-6">
+            Website Develop Planning
+          </h2>
+          <p>
+            Illo tempor unde ornare ipsum vivamus facilis? Tempus mollit secate
+            inn.
+          </p>
+        </div>
+        <div className="border relative border-gray-400 rounded-[2em] w-[16em] p-[2em]">
+          <div className="bg-[#F94C30] absolute top-[-10%] rounded-[1em] h-[4em] w-[4em]"></div>
+          <div className="flex justify-end">
+            <h1 className="text-[40px] font-bold">04.</h1>
+          </div>
+          <h2 className="font-bold text-[22px] mb-6">
+            Website Develop Planning
+          </h2>
+          <p>
+            Illo tempor unde ornare ipsum vivamus facilis? Tempus mollit secate
+            inn.
+          </p>
+        </div>
+      </section>
     </section>
   );
 }
