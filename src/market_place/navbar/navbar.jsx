6793ea02763ef2702d76bea09c57365e2063fafd
emdevelopa <gyimahsunday@gmail.com>
2024-06-04 23:30:03 +0100

    cool

--- src/market_place/navbar/navbar.jsx
+++ src/market_place/navbar/navbar.jsx
@@ -1,16 +1,16 @@
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { faHeadset } from "@fortawesome/free-solid-svg-icons/faHeadset";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Navbar = (props) => {
   return (
-    <nav className="flex justify-between">
+    <nav className="flex justify-between items-center text-zinc-50">
       <div>
         <img src={props.logo} alt="logo" className="w-[6em] " />
       </div>
       <div>
-        <ul >
+        <ul className="flex gap-8">
           <li>
             <a href="#">About</a>
           </li>
@@ -27,17 +27,27 @@ const Navbar = (props) => {
         </ul>
       </div>
       <div>
-        <ul>
+        <ul className="flex gap-8 items-center">
           <li>
             <a href="register">
               <FontAwesomeIcon icon={faHeadset} /> Live Chat
             </a>
           </li>
           <li>
-            <a href="#">Get Started</a>
+            <a
+              href="#"
+              className="bg-[#ff2d2d38] px-2 py-2 rounded-md hover:bg-black"
+            >
+              Get Started <FontAwesomeIcon icon={faUserCircle}/>
+            </a>
           </li>
-                  <li>
-                      <a href="#"><FontAwesomeIcon icon={faCartShopping}/> Cart</a>
+          <li>
+            <a
+              href="#"
+              className="bg-[#F94C30] px-2 py-2 rounded-sm hover:bg-black"
+            >
+              <FontAwesomeIcon icon={faCartShopping} /> Cart
+            </a>
           </li>
         </ul>
       </div>
--- src/market_place/page.jsx
+++ src/market_place/page.jsx
@@ -3,10 +3,10 @@ import Navbar from "./navbar/navbar";
 
 const MarketPlace = (props) => {
   return (
-    <section className="bg-[#131111] w-full">
+    <section className="bg-[#131111] w-full px-20">
       <Navbar logo={props.logo } />
 
-      <h1 className="text-red-400">MarketPlace</h1>
+      {/* <h1 className="text-red-400">MarketPlace</h1> */}
     </section>
   );
 };
