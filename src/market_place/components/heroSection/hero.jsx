ed8beb71242614fbc341c9efc95512610533bcab
emdevelopa <gyimahsunday@gmail.com>
2024-06-07 17:54:08 +0100

    cool

--- src/market_place/components/heroSection/hero.jsx
+++ src/market_place/components/heroSection/hero.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section  className="flex gap-4">
-      <div className="flex">
-        <h1 className="font-bold text-[34px]">GET A BEAUTIFUL PET FOR YOURSELF TODAY.</h1>
+    <section className="flex gap-4">
+      <div className="flex flex-col gap-8">
+        <h1 className="font-bold text-[34px]">
+          GET A BEAUTIFUL PET FOR YOURSELF TODAY.
+        </h1>
         <p>
           Get a pet from us today and discover a new companion. We sell at a
           very affordable rate. Reach out today
         </p>
         <div>
-          <button>EXPLORE</button>
+          <button className="p-[4em] font-bold">EXPLORE</button>
           <button>PURCHASE</button>
         </div>
       </div>
