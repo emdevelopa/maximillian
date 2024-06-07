ac9dcd6c3f9b2a7521a256cc4eb1b97bcbd2fbec
emdevelopa <gyimahsunday@gmail.com>
2024-06-07 17:51:19 +0100

    cool

---
+++ src/market_place/components/heroSection/hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section  className="flex gap-4">
+      <div className="flex">
+        <h1 className="font-bold text-[34px]">GET A BEAUTIFUL PET FOR YOURSELF TODAY.</h1>
+        <p>
+          Get a pet from us today and discover a new companion. We sell at a
+          very affordable rate. Reach out today
+        </p>
+        <div>
+          <button>EXPLORE</button>
+          <button>PURCHASE</button>
+        </div>
+      </div>
+      <div className="w-1/2"></div>
+    </section>
+  );
+};
+
+export default HeroSection;
